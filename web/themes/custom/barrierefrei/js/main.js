c0686c5e7b120370f785349bac5946aa2f5a12cb
Janet Lehmann <Janet.Lehmann@erdfisch.de>
2018-09-05 14:07:03 +0200

    Styling horizontal tabs and footer

--- web/themes/custom/barrierefrei/js/main.js
+++ web/themes/custom/barrierefrei/js/main.js
@@ -9,19 +9,35 @@
 (function (Drupal, $) {
   'use strict';
 
-  Drupal.behaviors.barrierefrei = {
+  Drupal.behaviors.barrierefreiNavBurger = {
     attach: function (context, settings) {
-      $(document).ready(function() {
+      // Check for click events on the navbar burger icon
+      $(".navbar-burger").click(function () {
 
-        // Check for click events on the navbar burger icon
-        $(".navbar-burger").click(function() {
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
 
-          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-          $(".navbar-burger").toggleClass("is-active");
-          $(".navbar-menu").toggleClass("is-active");
-
-        });
       });
+    }
+  };
+
+  Drupal.behaviors.barrierefreiHorizontalTabs = {
+    attach: function (context, settings) {
+
+      $('.paragraph--type--tab-item').hide();
+      $('.paragraph--type--tab-item').first().show();
+      $('.paragraphs--horizontal-tabs--navigation__item').first().addClass('is-active-tab');
+
+      var sourceId;
+      $('.paragraphs--horizontal-tabs--navigation__item').once('paragraphs--horizontal-tabs--navigation').click(function () {
+        sourceId = $(this).attr('data-tab-item-source');
+        $('.paragraph--type--tab-item').hide();
+        $('.is-active-tab').removeClass('is-active-tab');
+        $(this).addClass('is-active-tab');
+        $('[data-tab-item="' + sourceId + '"]').fadeIn();
+      });
+
     }
   };
 
